9665a114bef5bcb8f5d486523e84a702827f13c1
jdh767 <dhamukejabir@gmail.com>
2023-09-19 20:11:01 +0000

    third commit

--- fileprocessor.py
+++ fileprocessor.py
@@ -1,10 +1,7 @@
-a = 2
-b = 2
-c = a + b
-print("Python says: Hello, World!")
-print("%s + %s = %s" % (a,b,c))
-
-listOfUsers = ["User1", "User2", "User3"]
-
-for i in listOfUsers:
-        print(i)
+print("Printing out User Data:")
+f = open("fileprocessor.input", "r")
+line = f.readlines()
+for i in line:
+    i = i.split(":")
+    print("The user "+i[0] +" has a password of "+i[1] +" and has userid "+ i[2] +" and groupid "+i[3])
+print("End of User Data")
