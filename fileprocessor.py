7a5a052f1f5da55f41e593063b6b8641041c30ec
jdh767 <dhamukejabir@gmail.com>
2023-09-20 17:54:18 +0000

    fourth commit

--- fileprocessor.py
+++ fileprocessor.py
@@ -2,6 +2,9 @@ print("Printing out User Data:")
 f = open("fileprocessor.input", "r")
 line = f.readlines()
 for i in line:
-    i = i.split(":")
-    print("The user "+i[0] +" has a password of "+i[1] +" and has userid "+ i[2] +" and groupid "+i[3])
+	if (i.startswith("#") == False):
+		i = i.split(":")
+		print("The user "+i[0] +" has a password of "+i[1] +" and has userid "+ i[2] +" and groupid "+i[3])
+	else:
+		print("User4 is skipped because it starts with a hashtag (is commented out)")
 print("End of User Data")
---
+++ fileprocessor2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+
+for i in sys.stdin:
+     i = i.strip()
+     print (f'{i}')
